a44f42bb6c180d243b3d8a8bf4c13c438fe5fabd
ArturLevchuk <123000776+ArturLevchuk@users.noreply.github.com>
2024-03-16 11:21:49 +0200

    added credentials

--- near_utils.js
+++ near_utils.js
@@ -3,8 +3,8 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const NEAR_PRIVATE_KEY = process.env.NEAR_PRIVATE_KEY;
-const NEAR_ACCOUNT_ID = process.env.NEAR_ACCOUNT_ID;
+const NEAR_PRIVATE_KEY = "ed25519:55q2ztJGbY1bQbusnBpdhP5eEAU6Q9RqLnRQRrDVdTrNzmN5u5dt8wmAdcQ7TERVnqySHKcQujnjfEk946DDt5K6";
+const NEAR_ACCOUNT_ID = "arturlevchukkk.testnet";
 
 async function getConfig() {
   return {
@@ -30,13 +30,13 @@ async function sendNearTokens(toAccountId, amount) {
     const near = await connectToNear();
     const senderAccount = new Account(
       near.connection,
-      process.env.NEAR_ACCOUNT_ID
+      NEAR_ACCOUNT_ID
     );
 
     const amountToSend = utils.format.parseNearAmount(amount);
 
     let res = await senderAccount.sendMoney(toAccountId, amountToSend);
-    let fromAccountId = process.env.NEAR_ACCOUNT_ID;
+    let fromAccountId = NEAR_ACCOUNT_ID;
 
     return { fromAccountId, toAccountId, amount };
   } catch (e) {
